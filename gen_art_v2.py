e31f18ae4d0c8465a3e799772d696775d7ea08d9
Jude <judebrooks2023@gmail.com>
2025-02-26 10:19:28 -0600

    my brain is confused
    learning how radix sort lists work, and getting confused by the instructions

--- gen_art_v2.py
+++ gen_art_v2.py
@@ -14,6 +14,55 @@ def blue(scale=0.8):
     num = int(scale * 255)
     return (num // 2, 2 * num // 3, num)
 
+def rMaxLength(num):
+    maxDigits = 0
+    for n in num:
+        digCount = rGetLength(n)
+        if digCount > maxDigits:
+            maxDigits = digCount
+    return maxDigits
+
+def rGetLength(val):
+    if val == 0:
+        return 1
+    
+    digits = 0
+    while val != 0:
+        digits += 1
+        val = int(val/10)
+    return digits
+
+def radix_sort(numbers):
+    buckets = []
+    for i in range(10):
+        buckets.append([])
+    
+    # Find the max length, in number of digits
+    max_digits = rMaxLength(numbers)
+    
+    pow_10 = 1
+    for digit_index in range(max_digits):
+        for num in numbers:
+            bucket_index = (abs(num) // pow_10) % 10
+            buckets[bucket_index].append(num)
+
+        numbers.clear()
+        for bucket in buckets:
+            numbers.extend(bucket)
+            bucket.clear()
+      
+        pow_10 = pow_10 * 10
+    
+    negatives = []
+    non_negatives = []
+    for num in numbers:
+        if num < 0:
+            negatives.append(num)
+        else:
+            non_negatives.append(num)
+    negatives.reverse()
+    numbers.clear()
+    numbers.extend(negatives + non_negatives)
 
 class Node:
     def __init__(self, x, y, speed, angle):
@@ -79,6 +128,13 @@ for i in range(num_nodes):
     angle = radians(random.randint(0, 359))
     nodes.append(Node(x, y, speed, angle))
 
+#sort the nodes' x coordinates
+testListA = []
+for node in nodes:
+    testListA.append(node.x)
+p = sorted(testListA)
+print(p)
+
 # the game loop: (press q to quit)
 quit = False
 while not quit:
@@ -99,7 +155,8 @@ while not quit:
         node.reflect()
         node.draw()
 
-    
+
+
     for i, node1 in enumerate(nodes):
         x1, y1 = node1.x, node1.y
         for node2 in nodes[i + 1 :]:
